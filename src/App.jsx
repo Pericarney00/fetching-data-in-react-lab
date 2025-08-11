11fa2c180495110b46839b563237c9592c21be67
pericarney00 <pericarney@gmail.com>
2025-08-11 16:55:16 -0500

    Imported starshipcard into starshipList

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import { useState } from 'react'
-
+import * as starshipService from "./services/starshipService"
 import './App.css'
+import StarshipSearch from "./components/StarshipSearch/StarshipSearch"
+import StarshipList from "./components/StarshipList/StarshipList"
+
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  
+  const fetchData = async () => {
+    const data = await starshipService.show()
+    console.log("Data:", data)
+
+  }
+
+  return (
+    <main>
+      <h1>Star Wars API</h1>
+      <h2>Search</h2>
+      <h2>Starships</h2>
+
+
+
+    </main>
+  )
 };
 
 
--- src/services/starshipService.js
+++ src/services/starshipService.js
@@ -3,6 +3,9 @@ const BASE_URL = "https://swapi.info/api/starships/";
 const show = async () => {
   try {
     const res = await fetch(BASE_URL)
+    if (!res.ok) {
+      throw new Error("Failed to fetch starships.")
+    }
     const data = await res.json()
     console.log("Data:", data)
   } catch (error) {
@@ -11,4 +14,4 @@ const show = async () => {
 };
 
 
-export{index}
+export{show}
